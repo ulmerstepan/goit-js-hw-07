e5346db0e70b1404e88b25c8885d7b4dae38ad9d
Stepan Ulmer <ulmer.st@gmail.com>
2021-11-13 14:05:44 +0200

    task-1(without *)

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,41 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const imgsContainer = document.querySelector('.gallery');
+const imgsMarkup = createImgGallary(galleryItems);
+
+imgsContainer.insertAdjacentHTML('beforeend', imgsMarkup);
+
+function createImgGallary(gallaryData) {
+    return gallaryData.map(({ preview, original, description }) => {
+        return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+            </a>
+        </div>    
+        `;
+    })
+        .join('');
+
+};
+    
+imgsContainer.addEventListener('click', onGalleryImgClick);
+
+function onGalleryImgClick(evt) {
+
+    evt.preventDefault();
+    const isImgSwatchEl = evt.target.classList.contains('gallery__image');
+    if (!isImgSwatchEl) {
+        return;
+    }
+
+    basicLightbox.create(`
+		<img width="1400" height="900" src="${evt.target.dataset.source}">
+	`).show()
+};
